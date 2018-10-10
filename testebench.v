bdbfabaaea18080b76b359586a6187c719d2e272
Aryel Penido <39636625+AryelPenido@users.noreply.github.com>
2018-10-10 18:15:33 -0300

    Add files via upload

--- testebench.v
+++ testebench.v
@@ -3,16 +3,12 @@ module testbench();
      reg a,b,c,d,reset,ready;
      wire m1,m2,m3,m4,m5;
      codificador codificador_0(.a(a),.b(b),.c(c),.d(d),.reset(reset),.ready(ready));
-     initial begin
-      $dumpfile("teste.vcd");
-      $dumpvars(0, testbench);
-      $display("ola, eu sou um testbench");
-      $monitor("%b%b%b%b = %b%b%b%b%b", a, b, c, d, m1,m2,m3,m4,m5);
 
-
-     end
 
      initial begin //setando entrada com tabela verdade
+
+$dumpfile("codificador.vcd");
+      $dumpvars(0, testebench);
       reset=1;
       #1; reset=0;
       #1; ready=0;a=0; b=0; c=0; d=0; ready=1;//0000
